290d813e4e5dd1e8cdf0fe741506793e7aff8e79
eodhlwjr97 <99021614+eodhlwjr97@users.noreply.github.com>
2023-03-24 23:54:16 +0900

    0324_코드수정

--- 3월/BJ_2979_트럭주차.java
+++ 3월/BJ_2979_트럭주차.java
@@ -14,7 +14,7 @@ public class BJ_2979_트럭주차 {
 		// 수직선을 배열로 생각 EX) [1] = 1 ~ 2, [2] = 2 ~ 3
 		int[] time = new int[100];
 		int max = 0;
-  int min = 0;
+  		int min = 0;
 		for (int i = 0; i < 3; i++) { // 3대의 트럭이 들어옴
 			int start = in.nextInt(); // 입차시간
 			int finish = in.nextInt(); // 출차시간
@@ -22,8 +22,8 @@ public class BJ_2979_트럭주차 {
 			for (int t = 0; t < term; t++) {
 				time[start + t]++;
 			}
-   // 후에 배열 계산시 가지치기로 이용
-   min = Math.min(start, min);
+   			// 후에 배열 계산시 가지치기로 이용
+   			min = Math.min(start, min);
 			max = Math.max(finish, max); 
 		}
 		// System.out.println(Arrays.toString(time));
