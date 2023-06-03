70102762a0d57874c521effe60d3c6fdc317f833
eodhlwjr97 <99021614+eodhlwjr97@users.noreply.github.com>
2023-06-04 00:55:06 +0900

    0603

--- 6월/BJ_2096_내려가기.java
+++ 6월/BJ_2096_내려가기.java
@@ -3,65 +3,78 @@ package day_0603;
 import java.io.*;
 import java.util.*;
 
+/**
+ * Scanner : 		217900KB	920ms
+ * BufferedReader : 40136KB		316ms
+ * **/
+
 public class BJ_2096_내려가기 {
 	static int N;
-	static int[][] arr;
+//	static int[][] arr;
+
 	static int[] min;
 	static int[] max;
 
 //	static int[] dr = {1, 1, 1};
 //	static int[] dc = {-1, 0, 1};
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
 
-		N = in.nextInt(); // 3개씩 N줄
-//		arr = new int[N][N];
+		st = new StringTokenizer(br.readLine());
+		N = Integer.parseInt(st.nextToken()); // 3개씩 N줄
+//		arr = new int[N][N]; 
 //		for (int i = 0; i < N; i++) {
 //			for (int j = 0; j < N; j++) {
 //				arr[i][j] = in.nextInt();
 //			}
 //		}
+
 		min = new int[3];
 		max = new int[3];
 
+		st = new StringTokenizer(br.readLine(), " ");
 		// 첫째줄 입력받아서
 		for (int i = 0; i < 3; i++) {
-			max[i] = in.nextInt();
+			max[i] = Integer.parseInt(st.nextToken());
 			min[i] = max[i];
 		}
 
 		// 둘째줄부터 계산
 		for (int i = 1; i < N; i++) {
-			int x = in.nextInt();
-			int y = in.nextInt();
-			int z = in.nextInt();
+			st = new StringTokenizer(br.readLine(), " ");
+			int x = Integer.parseInt(st.nextToken()); // left 
+			int y = Integer.parseInt(st.nextToken()); // middle
+			int z = Integer.parseInt(st.nextToken()); // right
 
 			// 계산시 값이 변하므로 위의 값을 미리 백업
-			int origin1 = max[0];
-			int origin2 = max[1];
-			
+			// max
+			int up_left_origin = max[0];
+			int up_origin = max[1];
 
-			// (-1,0), (-1, 1) 비교
+			// x를 기준으로 (-1,0), (-1, 1) 비교 
 			max[0] = Math.max(max[0], max[1]) + x;
-//			min[0] = Math.min(min[0], min[1]) + x;
 
-			// (-1,-1), (-1, 0), (-1, 1) 비교
-			max[1] = Math.max(origin1, Math.max(max[1], max[2])) + y; // max[0]이 위에서 변경되므로 temp1값 이용
-//			min[1] = Math.min(origin1, Math.min(min[1], min[2])) + y;
+			// y를 기준으로 (-1,-1), (-1, 0), (-1, 1) 비교
+			max[1] = Math.max(up_left_origin, Math.max(max[1], max[2])) + y; // max[0]이 위에서 변경되므로 temp1값 이용
 
-			// (-1, 0), (-1, 1) 비교
-			max[2] = Math.max(origin2, max[2]) + z;
-//			min[2] = Math.min(origin2, min[2]) + z;
-			
-			origin1 = min[0];
-			origin2 = min[1];
-			origin3 = min[2];
-			
+			// z를 기준으로 (-1, 0), (-1, 1) 비교
+			max[2] = Math.max(up_origin, max[2]) + z;
+
+			// 계산시 값이 변하므로 위의 값을 미리 백업
+			// min (min에 대한 백업도 이루어져야 한다!)
+			up_left_origin = min[0];
+			up_origin = min[1];
+
+			// x를 기준으로 (-1,0), (-1, 1) 비교 
 			min[0] = Math.min(min[0], min[1]) + x;
-			min[1] = Math.min(origin1, Math.min(min[1], min[2])) + y;
-			min[2] = Math.min(origin2, min[2]) + z;
 
-//			System.out.println("\n" + Arrays.toString(min));
+			// y를 기준으로 (-1,-1), (-1, 0), (-1, 1) 비교
+			min[1] = Math.min(up_left_origin, Math.min(min[1], min[2])) + y;
+
+			// z를 기준으로 (-1, 0), (-1, 1) 비교
+			min[2] = Math.min(up_origin, min[2]) + z;
+
 		}
 
 		Arrays.sort(max);
