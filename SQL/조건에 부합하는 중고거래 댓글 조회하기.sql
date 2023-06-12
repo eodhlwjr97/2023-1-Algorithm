219266272a47f8dee53552fb55c59e30938b6cc6
eodhlwjr97 <99021614+eodhlwjr97@users.noreply.github.com>
2023-06-12 23:58:40 +0900

    0612_SELECT에서 AS 뒤 온점은 문자열로 취급한다

--- SQL/조건에 부합하는 중고거래 댓글 조회하기.sql
+++ SQL/조건에 부합하는 중고거래 댓글 조회하기.sql
@@ -1,5 +1,6 @@
 -- 코드를 입력하세요
  # SELECT B.TITLE, B.BOARD_ID, R.REPLY_ID, R.WRITER_ID, R.CONTENTS, DATE_FORMAT(R.CREATED_DATE, '%Y-%m-%d') AS R.CREATED_DATE
+ # SELECT B.TITLE, B.BOARD_ID, R.REPLY_ID, R.WRITER_ID, R.CONTENTS, DATE_FORMAT(R.CREATED_DATE, '%Y-%m-%d') AS 'R.CREATED_DATE'
  SELECT B.TITLE, B.BOARD_ID, R.REPLY_ID, R.WRITER_ID, R.CONTENTS, DATE_FORMAT(R.CREATED_DATE, '%Y-%m-%d') AS CREATED_DATE
  FROM USED_GOODS_BOARD AS B INNER JOIN USED_GOODS_REPLY AS R
  ON B.BOARD_ID = R.BOARD_ID
@@ -10,4 +11,4 @@
  # SQL 실행 중 오류가 발생하였습니다.
  # You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '.CREATED_DATE FROM USED_GOODS_BOARD AS B INNER JOIN USED_GOODS_REPLY AS R ON B' at line 2
  
- # 위 오류 이유 R.CREATED_DATE 가 아닌 CREATED_DATE로 써야한다. (R.CREATED_DATE는 테이블에서 내가 구분하기 쉽게 하기 위해 만든 컬럼명이지 실제로 테이블에서 R.CREATED_DATE로 존재하지 않는다)
+ # .(온점) 이 포함되면 문자열로 취급한다.
